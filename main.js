5cd81f6b87270629cd1b487f44ff9d44e696c0f0
panos-charalampopoulos <panos10cha@gmail.com>
2022-05-15 00:40:20 +0300

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+function getWeather() {
+    let city = document.getElementById("city").value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=25d9e242f7e2f0ac3889045fe2022153`)
+        .then((response) => response.json())
+        .then((weather) => {
+            let location = weather.weather[0].main;
+            let temp = weather.main.temp - 273.15;
+            document.getElementById("location").innerText = location;
+            document.getElementById("temp").innerText = temp;
+        });
+};
